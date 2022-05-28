1861d2f874440dbca1d64b5a6897b62aa546a0e0
gabrielweht <gabrielestebanw@gmail.com>
2022-05-28 19:31:06 -0300

    Models DB created

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -4,9 +4,56 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('videogame', {
+    id:{
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      unique: true,
+      validate: {
+        isUUID: 4
+      },
+      primaryKey: true
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    background_image: {
+      type: DataTypes.STRING
+    },
+    released: {
+      type:  DataTypes.DATEONLY,
+      defaultValue: DataTypes.NOW
+    },
+    rating: {
+      type: DataTypes.FLOAT,
+      validate: {
+        min: 1,
+        max: 5
+      }
+    },
+    platforms: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  },
+  {
+    timestamps: false
   });
+
+  sequelize.define('genre', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true
+    },
+    name: {
+      type: DataTypes.STRING
+    }
+  },
+  {
+    timestamps: false
+  })
 };
